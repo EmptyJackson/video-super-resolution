ad2a62e38b6002842456fe735ad9c50d352223f3
EmptyJackson <matthew@hmjackson.com>
2020-01-09 11:10:02 +0000

    Add FSRCNN architecture implementation
    Signed-off-by: EmptyJackson <matthew@hmjackson.com>

---
+++ src/models/fsrcnn.py
@@ -0,0 +1,81 @@
+import tensorflow as tf
+from tensorflow.python.keras.models import Model
+
+def fsrcnn(x,
+           (d, s, m),
+           scale=2,
+           channels=3):
+    """
+    Architecture from original paper: https://arxiv.org/pdf/1608.00367.pdf
+    """
+
+    in_shape = tf.shape(x)
+
+    filters = [
+        tf.Variable(tf.random_normal([5, 5, 1, d], stddev=0.1), name="fil1"),
+        tf.Variable(tf.random_normal([1, 1, d, s], stddev=0.1), name="fil2")
+    ]
+    for i in range(m):
+        filters.append(tf.random_normal([3, 3, s, s], stddev=0.1), name="fil"+str(fid+3))
+    filters.append(tf.random_normal([1, 1, s, d], stddev=0.1), name="fil"+str(m+3))
+    filters.append(tf.random_normal([9, 9, 1, d], stddev=0.1), name="fil"+str(m+4))
+    
+    bias = 
+        tf.get_variable(shape=[d], initializer=bias_initializer, name="bias1"),
+        tf.get_variable(shape=[s], initializer=bias_initializer, name="bias2")
+    ]
+    for i in range(m):
+        bias.append(tf.get_variable(shape=[s], initializer=bias_initializer, name="bias"+str(i+3)))
+    bias.append(tf.get_variable(shape=[d], initializer=bias_initializer, name="bias"+str(m+3)))
+    bias.append(tf.get_variable(shape=[1], initializer=bias_initializer, name="bias"+str(m+4)))
+
+    with tf.name_scope("feature_extraction"):
+        x = tf.layers.conv2d(
+            x,
+            filters=filters[0],
+            padding="same",
+            name="conv")
+        x = x + bias[0]
+        x = tf.nn.relu(x, name="relu")
+
+    with tf.name_scope("shrinking"):
+        x = tf.layers.conv2d(
+            x,
+            filters=filters[1],
+            padding="same",
+            name="conv")
+        x = x + bias[1]
+        x = tf.nn.relu(x, name="relu")
+
+    with tf.name_scope("non-linear_mapping"):
+        for i in range(m):
+                x = tf.layers.conv2d(
+                x,
+                filters=filters[i+2],
+                padding="same",
+                name="conv"+str(i+1))
+            x = x + bias[i+2]    
+        x = tf.nn.relu(x, name="relu")
+
+    with tf.name_scope("expanding"):
+        x = tf.layers.conv2d(
+            x,
+            filters=filters[m+2],
+            padding="same",
+            name="conv")
+        x = tf.nn.bias_add(x, bias[m+2]
+        x = tf.nn.relu(x, name="relu")
+
+    with tf.name_scope("deconvolution"):
+        x = tf.layers.conv2d_transpose(
+            x,
+            filters=filters[m+3],
+            output_shape=[1, in_shape*scale, in_shape*scale, channels],
+            strides=[1, scale, scale, 1]
+            padding="same",
+            name="transpose_conv"
+        )
+
+    # TODO: Include bias add here?
+
+    return x
