8630cbcf6bfd54673cc46e6ac05d5e74c625ca52
EmptyJackson <matthew@hmjackson.com>
2020-02-29 12:21:43 +0000

    Add scale option to crop_image

--- src/tools/crop_image.py
+++ src/tools/crop_image.py
@@ -6,34 +6,37 @@ from PIL import Image
 RESOLUTIONS = { 240: 352, 360: 480, 480: 858, 720: 1280, 1080: 1920 }
 
 def main():
-    if len(sys.argv) != 4 and len(sys.argv) != 5:
-        print("Usage: crop_image.py <input path> <output path> <vertical resolution> [shape]\n")
+    if len(sys.argv) < 4 or len(sys.argv) > 6:
+        print("Usage: crop_image.py <input path> <output path> <vertical resolution> [shape] [scale]\n")
         exit()
 
     in_path = sys.argv[1]
     out_path = sys.argv[2]
     res = int(sys.argv[3])
     shape = "standard"
-    if len(sys.argv) == 5:
+    scale = 1
+    if len(sys.argv) >= 5:
         shape = sys.argv[4]
+    if len(sys.argv) == 6:
+        scale = int(sys.argv[5])
 
     if shape == 'standard' and not res in RESOLUTIONS:
         raise ValueError("Resolution must be standard (240, 360, 480, 720, 1080)")
 
     im = Image.open(in_path)
     w, h = im.size
-    hs = (h - res) / 2
+    hs = (h - scale*res) / 2
     if shape == 'standard':
-        ws = (w - RESOLUTIONS[res]) / 2
+        ws = (w - scale*RESOLUTIONS[res]) / 2
     elif shape == 'square':
-        ws = (w - res) / 2
+        ws = (w - scale*res) / 2
     else:
         raise ValueError("Shape " + shape + " not recognised.")
 
     if shape == 'standard':
-        cropped_im = im.crop((ws, hs, ws+RESOLUTIONS[res], hs+res))
+        cropped_im = im.crop((ws, hs, ws+scale*RESOLUTIONS[res], hs+scale*res))
     elif shape == 'square':
-        cropped_im = im.crop((ws, hs, ws+res, hs+res))
+        cropped_im = im.crop((ws, hs, ws+scale*res, hs+scale*res))
     cropped_im.save(out_path)
 
 
